cd156033f0f7561092f58e692c280ce5e3a7e1c4
joonson22 <johnsonlai22@gmail.com>
2019-10-03 15:59:27 -0700

    Save before trying to create levels

--- src/explosion.js
+++ src/explosion.js
@@ -12,18 +12,27 @@ const DEFAULTS = {
 };
 
 function Explosion(options) {
-    options = options || {};
-    options.color = DEFAULTS.COLOR;
-    options.pos = DEFAULTS.pos;
-    options.width = DEFAULTS.width;
-    options.height = DEFAULTS.height;
-    StillObject.call(this, options);
+    this.color = options.color
+    this.pos = options.pos
+    this.width = options.width
+    this.height = options.height
+    // options = options || {};
+    // options.color = DEFAULTS.COLOR;
+    // options.pos = DEFAULTS.pos;
+    // options.width = DEFAULTS.width;
+    // options.height = DEFAULTS.height;
+    // StillObject.call(this, options);
     // this.pulse = this.pulse.bind(this);
     this.active = true;
     this.timer = 70;
 }
 
-Util.inherits(Explosion, StillObject);
+// Util.inherits(Explosion, StillObject);
+Explosion.prototype.draw = function (ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.pos[0], this.pos[1], this.width, this.height);
+    ctx.fill();
+}
 
 Explosion.prototype.update = function (deltaTime) {
     
